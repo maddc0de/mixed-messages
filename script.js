72c1ea1dee3131bda6029bf8f671723fc15a31ab
maddc0de <maddc0de@outlook.com>
2022-01-23 16:26:51 +0000

    randomise dates

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+//pick random element from array
+const random = arr => arr[Math.floor(Math.random() * arr.length)];
+
 const dates = ['next month', 'this year', 'someday', 'in the future' ];
 
 const noPredictions = [
@@ -12,24 +15,24 @@ const noPredictions = [
 ];
 
 const predictions = [
-    `life won't always be this miserable. You'll get through this, ${dates}. Maybe..`,
-    `you will receive a huge present ${dates} -a box of regrets`,
-    `your luck will change ${dates} -for the worse`,
-    `hope and dreams are definitely possible to achieve ${dates} -just not for you.`,
-    `${dates}, someone will find you attractive. That someone is obviously lying.`,
-    `you will find your purpose ${dates} -as a bad example.`,
-    `happinesss is just around the corner ${dates} for you. too bad, the earth is round.`,
-    `${dates}, You are going to cry so much, nothing new I know but that\'s ok lol`
+    `life won't always be this miserable. You'll get through this, ${random(dates)}. Maybe..`,
+    `you will receive a huge present ${random(dates)} -a box of regrets`,
+    `your luck will change ${random(dates)} -for the worse`,
+    `hope and dreams are definitely possible to achieve ${random(dates)} -just not for you.`,
+    `${random(dates)}, someone will find you attractive. That someone is obviously lying.`,
+    `you will find your purpose ${random(dates)} -as a bad example.`,
+    `happinesss is just around the corner ${random(dates)} for you. too bad, the earth is round.`,
+    `${random(dates)}, You are going to cry so much, nothing new I know but that\'s ok lol`
 ];
 
-// pseudo-random boolean that decides if user receives a prediction or not
-const decide = Math.random() 
-console.log(decide);
 
 const randomNoP = Math.floor(Math.random() * noPredictions.length);
 const randomP = Math.floor(Math.random() * predictions.length);
 
+// pseudo-random boolean that decides if user receives a prediction or not
+const decide = Math.random() 
 const fortuneDecision = num => num <= 0.4999 ? console.log(predictions[randomNoP]) : console.log(noPredictions[randomP]);
+
 fortuneDecision(decide);
 
 
